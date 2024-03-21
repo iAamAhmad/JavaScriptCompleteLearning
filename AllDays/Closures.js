e9fc60e5408d37d04d7524a701cb1acbbc249e7c
iAamAhmad <aghazi1011@gmail.com>
2024-03-22 04:48:17 +0500

    closures

---
+++ AllDays/Closures.js
@@ -0,0 +1,10 @@
+function outerFunction () {
+   const outerVariable = 'i am variabke of outerFunction'
+   function innerFunction(){
+      console.log(outerVariable)
+      return 'Value returned from innerFunction';
+   }
+   return innerFunction;
+}
+const testedFunction = outerFunction();
+console.log(testedFunction());
