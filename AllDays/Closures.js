fb428e99b1125367c2165c4e21042fe990fc92d6
iAamAhmad <aghazi1011@gmail.com>
2024-03-23 05:51:09 +0500

    first commit

--- AllDays/Closures.js
+++ AllDays/Closures.js
@@ -8,3 +8,15 @@ function outerFunction () {
 }
 const testedFunction = outerFunction();
 console.log(testedFunction());
+
+
+function outerFunction () {
+   const outerVariable = 'i am variabke of outerFunction'
+   function innerFunction(){
+      console.log(outerVariable)
+      return 'Value returned from innerFunction';
+   }
+   return innerFunction;
+}
+const testedFunctionone = outerFunction();
+console.log(testedFunction());
